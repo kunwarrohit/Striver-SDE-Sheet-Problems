03677ed5a7b74f8959e06892567afc97ae9c0f58
Kunwar Randhir Singh <62481165+kunwarrohit@users.noreply.github.com>
2022-06-27 23:07:02 +0530

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// Driver Code
+int main(){
+
+    long int n=3;
+
+	for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            for(int k=0;k<n;k++){
+                cout<<"Hello"<<" "<<"World!";
+            }
+            cout<<endl;
+        }cout<<endl;
+    }
+
+return 0;
+}
+
+// This code has been contributed by Striver
